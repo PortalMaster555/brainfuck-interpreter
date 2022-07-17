93ac9896e43e00b5541060c6434742bcd548db1e
PortalMaster555 <portalmaster55@gmail.com>
2022-07-16 22:59:32 -0500

    program now sees only bf chars, ignores all else as comments

---
+++ main.bf
@@ -0,0 +1,4 @@
+THIS PROGRAM +++++,.,.,.sdfdsf,/.,,./,,.sdf,=,dsf.=,..,-.,-.,<><>
+
+ONLY SEES dfd+++_<><,.,][[, VALID BRAINFUCK
+++---+++_hqwudhqeu37328jdshfhsudf CHARACTERS 
--- main.c
+++ main.c
@@ -3,15 +3,33 @@
 int main(int argc, char **argv)
 {
 
+	char charSet[8] = "><+-.,[]";
+
 	char *bfFilename = "main.bf";
 	char *outputFilename = "out.txt";
 	
 	FILE *bfCode = NULL;
 	FILE *outputFile;
 
+	char bfBuffer[1024];
+
 	bfCode = fopen(bfFilename, "r");
 	outputFile = fopen(outputFilename, "w");
 
+	char ch;
+	int buffSize = 0;
+	while((ch = fgetc(bfCode)) != EOF)
+	{
+		for(int j = 0; j < 8; j++)
+		{
+			if(ch == charSet[j])
+				bfBuffer[buffSize++] = ch;
+		}	
+	}
+	if(feof(bfCode))
+		bfBuffer[buffSize++] = '\0';
+	
+	printf("%s\n", bfBuffer);
 	fclose(bfCode);
 	fclose(outputFile);
 
