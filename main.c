baf21a6152df5c8ad0cab0ff168fb6cc73cc486f
PortalMaster555 <portalmaster55@gmail.com>
2022-07-16 22:10:25 -0500

    file open check done

--- main.c
+++ main.c
@@ -2,10 +2,25 @@
 
 int main(int argc, char **argv)
 {
-	for(int i = 0; i < argc; i++)
+
+	char *filename = "main.bf";
+	
+	FILE *bfCode = NULL;
+	FILE *outputFile;
+	int openAttempts = 5;
+	int attempt = 0;
+
+	while(bfCode == NULL)
 	{
-		printf("%s ", argv[i]);
+		bfCode = fopen(filename, "r");
+		if(++attempt == openAttempts)
+		{
+			printf("Failed to open file \"%s\" in %d attempts.\n", filename, openAttempts);
+			return 1;
+		}
 	}
-	printf("\n");
+
+	fclose(bfCode);
+	fclose(outputFile);
 	return 0;
 }
