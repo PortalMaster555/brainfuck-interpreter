2e5db5a8260e88897e8459c0bed38ae796ea20e9
PortalMaster555 <portalmaster55@gmail.com>
2022-07-16 22:23:52 -0500

    removed check, is redundant

--- main.c
+++ main.c
@@ -3,24 +3,17 @@
 int main(int argc, char **argv)
 {
 
-	char *filename = "main.bf";
+	char *bfFilename = "main.bf";
+	char *outputFilename = "out.txt";
 	
 	FILE *bfCode = NULL;
 	FILE *outputFile;
-	int openAttempts = 5;
-	int attempt = 0;
 
-	while(bfCode == NULL)
-	{
-		bfCode = fopen(filename, "r");
-		if(++attempt == openAttempts)
-		{
-			printf("Failed to open file \"%s\" in %d attempts.\n", filename, openAttempts);
-			return 1;
-		}
-	}
+	bfCode = fopen(bfFilename, "r");
+	outputFile = fopen(outputFilename, "w");
 
 	fclose(bfCode);
 	fclose(outputFile);
+
 	return 0;
 }
